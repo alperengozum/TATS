fa7ff269e9e0cf2a17107d03837d7ed7daba7c90
serhat_derya <88051030+SerhatDerya@users.noreply.github.com>
2024-12-20 02:15:58 +0300

    foodCategoryModel improvement 7

--- ai/foodCategoryModel_train/test.py
+++ ai/foodCategoryModel_train/test.py
@@ -1,27 +1,48 @@
 import os
 from ultralytics import YOLO
 import cv2
+import math
 
-model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', 'last.pt')
+model_path = os.path.join('.', 'runs', 'detect', 'train8', 'weights', 'last.pt')
 
 model = YOLO(model_path)
 
-image_path = "data/datasets/images/train/9.jpeg"
-
+#image_path = "data/datasets/train/images/4_jpeg.rf.eaa1ad931717afbb6681fc92729f7106.jpg"
+image_path = "data/old_datasets/images/train/4.jpeg"
 
 img = cv2.imread(image_path)
-
+h,w=img.shape[:2]
 
 threshold = 0.5
 results = model(img)[0]
-
+# Define colors for each class
+class_colors = {
+    0: (0, 255, 0),    # Green for class 0
+    1: (255, 0, 0),    # Blue for class 1
+    2: (0, 0, 255),    # Red for class 2
+    3: (255, 255, 0)   # Cyan for class 3
+}
+i=0
 # Process results
-i = 0
 for result in results.boxes.data.tolist():
     x1, y1, x2, y2, score, class_id = result
     if score > threshold:
-        cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-        cv2.putText(img, model.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
-        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+        # Text settings
+        text = model.names[int(class_id)]
+        font = cv2.FONT_HERSHEY_DUPLEX
+        font_scale = w / 1200  # 1.1
+        font_thickness = math.floor(w / 700)  # 2
+        text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+        text_w, text_h = text_size
+
+        color = class_colors.get(int(class_id), (0, 255, 0))  # Default to green if class_id not found
+        cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 0), font_thickness*5)
+        cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), color, font_thickness*2)
+
+        # Text with outline
+        outline_color = (0, 0, 0)  # Black outline
+        cv2.putText(img, text, (int(x1), int(y1 - 10)), font, font_scale, outline_color, 3*font_thickness ,
+                    cv2.LINE_AA)
+        cv2.putText(img, text, (int(x1), int(y1 - 10)), font, font_scale, color, font_thickness, cv2.LINE_AA)
         i += 1
 cv2.imwrite(f"results/res.jpg", img)
--- ai/foodCategoryModel_train/train.py
+++ ai/foodCategoryModel_train/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8s.pt")
 
 # Use the model
-results = model.train(data="config.yaml", epochs=50)  # train the model
+results = model.train(data="config.yaml", epochs=60, imgsz=640, batch=16, device="mps")  # train the model
